9d670572b83c233ffcac1217bc97f1ab9c978b21
Daniel Khalil <d.khalil@brainsware.org>
2015-11-04 15:57:06 +0100

    test(model): moved model initialization to global space and added relations for testing

--- spec/modelSpec.js
+++ spec/modelSpec.js
@@ -1,34 +1,62 @@
-var Responses;
+var Model, MultiSubModel, Responses, SubModel, co;
 
 Responses = {
   general: {
     status: 200,
-    responseText: '{"id": "1"}'
+    responseText: '{"id": 1, "submodel_id": 1}'
   },
   generalMulti: {
     status: 200,
-    responseText: '[{"id": "1"}]'
+    responseText: '[{"id": 1}]'
   }
 };
+
+co = new Connector('/');
+
+Model = Modelize({
+  api: 'tests',
+  connector: co,
+  has_one: {
+    submodel: {
+      model: 'SubModel'
+    }
+  },
+  has_many: {
+    multisubmodel: {
+      model: 'MultiSubModel'
+    }
+  },
+  editable: ['fieldOne', 'fieldTwo']
+});
+
+SubModel = Modelize({
+  api: 'subtests',
+  connector: co
+});
+
+MultiSubModel = Modelize({
+  api: 'multisubtests',
+  connector: co,
+  belongs_to: {
+    tests: {
+      model: 'Model'
+    }
+  }
+});
 
 describe('Model', function() {
   beforeAll(function() {
     jasmine.Ajax.install();
-    this.connector = new Connector('/');
-    this.Model = Modelize({
-      api: 'tests',
-      connector: this.connector
-    });
-    return this.instance = new this.Model();
+    return this.instance = new Model();
   });
   it('is a function', function() {
-    return expect(typeof this.Model).toBe('function');
+    return expect(typeof Model).toBe('function');
   });
   it('gets and calls a function', function() {
     var request;
-    this.Model.get({}, function(data) {
+    Model.get({}, function(data) {
       expect(typeof data).toBe('object');
-      return expect(data.id).toBe('1');
+      return expect(data.id).toBe(1);
     });
     request = jasmine.Ajax.requests.mostRecent();
     expect(request.url).toBe('/tests');
@@ -40,19 +68,19 @@ describe('Model', function() {
     var request;
     ObservableArray(this, 'test');
     spyOn(this.test, 'push');
-    this.Model.get({}, this.test);
+    Model.get({}, this.test);
     request = jasmine.Ajax.requests.mostRecent();
     request.respondWith(Responses.generalMulti);
     expect(this.test.push).toHaveBeenCalled();
     return expect(this.test.push).toHaveBeenCalledWith(jasmine.objectContaining({
-      id: '1'
+      id: 1
     }));
   });
   it('creates and calls a function', function() {
     var request;
-    this.Model.create({}, function(data) {
+    Model.create({}, function(data) {
       expect(typeof data).toBe('object');
-      return expect(data.id).toBe('1');
+      return expect(data.id).toBe(1);
     });
     request = jasmine.Ajax.requests.mostRecent();
     expect(request.url).toBe('/tests');
@@ -64,17 +92,33 @@ describe('Model', function() {
     var request;
     ObservableArray(this, 'test');
     spyOn(this.test, 'push');
-    this.Model.create({}, this.test);
+    Model.create({}, this.test);
     request = jasmine.Ajax.requests.mostRecent();
     request.respondWith(Responses.general);
     expect(this.test.push).toHaveBeenCalled();
     return expect(this.test.push).toHaveBeenCalledWith(jasmine.objectContaining({
-      id: '1'
+      id: 1
     }));
   });
-  return describe('Instance', function() {
-    return it('is an object', function() {
+  return describe('Instance functions', function() {
+    beforeAll(function() {
+      var request;
+      Model.get_one(1, (function(_this) {
+        return function(data) {
+          return _this.instance = new Model(data);
+        };
+      })(this));
+      request = jasmine.Ajax.requests.mostRecent();
+      return request.respondWith(Responses.general);
+    });
+    it('is an object', function() {
       return expect(typeof this.instance).toBe('object');
+    });
+    it('has an id', function() {
+      return expect(this.instance.id).toBe(1);
+    });
+    return it('has external keys', function() {
+      return expect(this.instance.submodel_id()).toBe(1);
     });
   });
 });
--- spec_src/modelSpec.coffee
+++ spec_src/modelSpec.coffee
@@ -1,30 +1,48 @@
 Responses =
   general:
     status: 200
-    responseText: '{"id": "1"}'
+    responseText: '{"id": 1, "submodel_id": 1}'
   generalMulti:
     status: 200
-    responseText: '[{"id": "1"}]'
+    responseText: '[{"id": 1}]'
+
+co = new Connector('/')
+
+Model = Modelize
+  api: 'tests'
+  connector: co
+  has_one:
+    submodel:
+      model: 'SubModel'
+  has_many:
+    multisubmodel:
+      model: 'MultiSubModel'
+  editable: [ 'fieldOne', 'fieldTwo' ]
+
+SubModel = Modelize
+  api: 'subtests'
+  connector: co
+
+MultiSubModel = Modelize
+  api: 'multisubtests'
+  connector: co
+  belongs_to:
+    tests:
+      model: 'Model'
 
 describe 'Model', ->
   beforeAll ->
     jasmine.Ajax.install()
 
-    @connector = new Connector('/')
-
-    @Model = Modelize
-      api: 'tests'
-      connector: @connector
-
-    @instance = new @Model()
+    @instance = new Model()
 
   it 'is a function', ->
-    expect(typeof @Model).toBe 'function'
+    expect(typeof Model).toBe 'function'
 
   it 'gets and calls a function', ->
-    @Model.get {}, (data) ->
+    Model.get {}, (data) ->
       expect(typeof data).toBe 'object'
-      expect(data.id).toBe '1'
+      expect(data.id).toBe 1
 
     request = jasmine.Ajax.requests.mostRecent()
 
@@ -38,18 +56,18 @@ describe 'Model', ->
     ObservableArray @, 'test'
     spyOn @test, 'push'
 
-    @Model.get {}, @test
+    Model.get {}, @test
 
     request = jasmine.Ajax.requests.mostRecent()
     request.respondWith Responses.generalMulti
 
     expect(@test.push).toHaveBeenCalled()
-    expect(@test.push).toHaveBeenCalledWith jasmine.objectContaining({ id: '1' })
+    expect(@test.push).toHaveBeenCalledWith jasmine.objectContaining({ id: 1 })
 
   it 'creates and calls a function', ->
-    @Model.create {}, (data) ->
+    Model.create {}, (data) ->
       expect(typeof data).toBe 'object'
-      expect(data.id).toBe '1'
+      expect(data.id).toBe 1
 
     request = jasmine.Ajax.requests.mostRecent()
 
@@ -63,14 +81,27 @@ describe 'Model', ->
     ObservableArray @, 'test'
     spyOn @test, 'push'
 
-    @Model.create {}, @test
+    Model.create {}, @test
 
     request = jasmine.Ajax.requests.mostRecent()
     request.respondWith Responses.general
 
     expect(@test.push).toHaveBeenCalled()
-    expect(@test.push).toHaveBeenCalledWith jasmine.objectContaining({ id: '1' })
+    expect(@test.push).toHaveBeenCalledWith jasmine.objectContaining({ id: 1 })
 
-  describe 'Instance', ->
+  describe 'Instance functions', ->
+    beforeAll ->
+      Model.get_one 1, (data) =>
+        @instance = new Model(data)
+
+      request = jasmine.Ajax.requests.mostRecent()
+      request.respondWith Responses.general
+
     it 'is an object', ->
       expect(typeof @instance).toBe 'object'
+
+    it 'has an id', ->
+      expect(@instance.id).toBe 1
+
+    it 'has external keys', ->
+      expect(@instance.submodel_id()).toBe 1
