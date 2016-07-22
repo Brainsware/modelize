c5acad53fe2bb32971267012e30d1649dead9d50
Daniel Khalil <d.khalil@brainsware.org>
2016-07-22 19:06:44 +0200

    test(container): added multi type container tests

--- spec/containerSpec.js
+++ spec/containerSpec.js
@@ -1,25 +1,39 @@
-var Post, PostData, co;
-
-PostData = Container({
-  editable: ['title', 'content']
-});
-
-co = new RESTConnector('/');
-
-Post = Modelize({
-  api: 'posts',
-  connector: co,
-  container: {
-    PostData: {}
-  }
-});
-
 describe('Container', function() {
   beforeAll(function() {
-    this.container_instance = new PostData();
-    return this.post_instance = new Post();
+    var Comment, Post, PostData, co;
+    PostData = Container({
+      editable: ['title', 'content']
+    });
+    Comment = Container({
+      editable: ['title', 'content']
+    });
+    co = new RESTConnector('/');
+    Post = Modelize({
+      api: 'posts',
+      connector: co,
+      container: {
+        PostData: {
+          first_class: true,
+          datahandler: new JSONHandler(),
+          container: PostData
+        },
+        comments: {
+          type: 'multi',
+          datahandler: new JSONHandler(),
+          container: Comment
+        }
+      }
+    });
+    return this.post = new Post();
   });
-  return it('is a function', function() {
-    return expect(typeof PostData).toBe('function');
+  it('has single container', function() {
+    return expect(typeof this.post.PostData).toBe('function');
+  });
+  return it('can add comments', function() {
+    expect(typeof this.post.comments_add).toBe('function');
+    return this.post.comments_add({
+      title: 'Test Title',
+      conent: 'Test Content'
+    });
   });
 });
--- spec_src/containerSpec.coffee
+++ spec_src/containerSpec.coffee
@@ -1,21 +1,40 @@
-PostData = Container
-  editable: [
-    'title',
-    'content'
-  ]
-
-co = new RESTConnector('/')
-
-Post = Modelize
-  api: 'posts'
-  connector: co
-  container:
-    PostData: {}
-
 describe 'Container', ->
   beforeAll ->
-    @container_instance = new PostData()
-    @post_instance = new Post()
+    PostData = Container
+      editable: [
+        'title',
+        'content'
+      ]
 
-  it 'is a function', ->
-    expect(typeof PostData).toBe 'function'
+    Comment = Container
+      editable: [
+        'title',
+        'content'
+      ]
+
+    co = new RESTConnector('/')
+
+    Post = Modelize
+      api: 'posts'
+      connector: co
+      container:
+        PostData:
+          first_class: true
+          datahandler: new JSONHandler()
+          container:   PostData
+        comments:
+          type:        'multi'
+          datahandler: new JSONHandler()
+          container:   Comment
+
+    @post = new Post()
+
+  it 'has single container', ->
+    expect(typeof @post.PostData).toBe 'function'
+
+  it 'can add comments', ->
+    expect(typeof @post.comments_add).toBe 'function'
+
+    @post.comments_add
+      title:  'Test Title'
+      conent: 'Test Content'
