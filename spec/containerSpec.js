3b87495570bdd94acdc1e1cdd9272e6a20f051e5
Daniel Khalil <d.khalil@brainsware.org>
2017-02-27 20:06:45 +0100

    chore: compile

--- spec/containerSpec.js
+++ spec/containerSpec.js
@@ -29,11 +29,35 @@ describe('Container', function() {
   it('has single container', function() {
     return expect(typeof this.post.PostData).toBe('function');
   });
-  return it('can add comments', function() {
+  it('has first class objects from container', function() {
+    expect(typeof this.post.title).toBe('function');
+    expect(typeof this.post.content).toBe('function');
+    this.post.title('Post Title');
+    return this.post.content('Post Content');
+  });
+  it('can add comments', function() {
     expect(typeof this.post.comments_add).toBe('function');
     return this.post.comments_add({
       title: 'Test Title',
-      conent: 'Test Content'
+      content: 'Test Content'
     });
+  });
+  return it('exports containers', function() {
+    var data;
+    data = this.post["export"]();
+    expect(data).toEqual(jasmine.objectContaining({
+      PostData: {
+        title: 'Post Title',
+        content: 'Post Content'
+      }
+    }));
+    return expect(data).toEqual(jasmine.objectContaining({
+      comments: [
+        {
+          title: 'Test Title',
+          content: 'Test Content'
+        }
+      ]
+    }));
   });
 });
